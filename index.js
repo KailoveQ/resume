4201982e2683f0abe8fbd423c0330ec9caba2381
Kai <1114366961@qq.com>
2021-04-15 10:15:43 +0800

    完善H5小程序预览模式

--- index.js
+++ index.js
@@ -10,20 +10,23 @@ const oldTimes = document.querySelector("#old-times")
 const qrcode_oldTimes = document.querySelector(".old-times")
 const width = document.documentElement.clientWidth
 
-function previewQrCode(target, show) {
-    target.addEventListener("mouseenter", () => {
+function previewQrCode(target, show,event1,event2) {
+    target.addEventListener(event1, () => {
         show.classList.add("show")
     })
-    target.addEventListener("mouseleave", () => {
+    target.addEventListener(event2, () => {
         if (Array.from(show.classList).indexOf("show") >= 0) {
             show.classList.remove("show");
         }
     })
 }
+
 if (width > 500) {
-    previewQrCode(previewVue, qrcode_vue)
-    previewQrCode(doraemon, qrcode_doraemon)
-    previewQrCode(canvas, qrcode_canvas)
-    previewQrCode(dingdangUi, qrcode_dingdang)
-    previewQrCode(oldTimes, qrcode_oldTimes)
+    previewQrCode(previewVue, qrcode_vue,"mouseenter","mouseleave")
+    previewQrCode(doraemon, qrcode_doraemon,"mouseenter","mouseleave")
+    previewQrCode(canvas, qrcode_canvas,"mouseenter","mouseleave")
+    previewQrCode(dingdangUi, qrcode_dingdang,"mouseenter","mouseleave")
+    previewQrCode(oldTimes, qrcode_oldTimes,"mouseenter","mouseleave")
+}else{
+    previewQrCode(oldTimes, qrcode_oldTimes,"tab","touchstart")
 }
